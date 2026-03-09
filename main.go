0042b2ddc122dd37f43dd2b90f50020fd866c5b8
Shenali <shenalijayakody@gmail.com>
2026-03-10 00:41:55 +0530

    Add api server

--- main.go
+++ main.go
@@ -1,18 +1,75 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"maskit/mask"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "serve" {
+		runServer()
+		return
+	}
+	runCLI()
+}
+
+func runCLI() {
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	fmt.Print(mask.Mask(string(input)))
+}
+
+func runServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/mask", handleMask)
+
+	addr := ":8080"
+	log.Printf("maskit server listening on %s", addr)
+	if err := http.ListenAndServe(addr, withCORS(mux)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handleMask(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req struct {
+		Text string `json:"text"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	masked := mask.Mask(req.Text)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(struct {
+		Masked string `json:"masked"`
+	}{Masked: masked})
 }
